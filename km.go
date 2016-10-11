59a125a1e7f768480cd9c7b579e93b1e358a9fe8
devcy <penguendev@gmail.com>
2016-10-11 15:41:37 +0300

    before ui

--- km.go
+++ km.go
@@ -3,10 +3,23 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/net/html"
+)
+
+const (
+	mainline     = "http://kernel.ubuntu.com/~kernel-ppa/mainline/"
+	downloadPath = "/tmp/kernelz"
 )
 
 //write err
@@ -147,8 +160,145 @@ func DisplayMenu() {
 	}
 }
 
+//GrabMainLinks parse html and grab href
+func GrabMainLinks(u string) (links []string) {
+	if u == "" {
+		u = mainline
+	}
+	resp, err := http.Get(u)
+	defer resp.Body.Close()
+	pe(err)
+
+	tk := html.NewTokenizer(resp.Body)
+
+	for t := tk.Next(); t != html.ErrorToken; {
+
+		if t == html.StartTagToken {
+			for _, a := range tk.Token().Attr {
+				if a.Key == "href" {
+					if u == mainline {
+						if strings.Index(a.Val, "v4") > -1 { // only v4+
+							links = append(links, mainline+a.Val)
+						}
+					} else {
+						links = append(links, u+a.Val)
+
+					}
+
+				}
+			}
+		}
+		t = tk.Next()
+	}
+
+	return links
+}
+
+//DownloadKernel downlods deb files
+func DownloadKernel() {
+	var downloadlist []string
+	links := GrabMainLinks("")
+	cls()
+
+	for index, k := range links {
+		ver := strings.Replace(strings.Replace(k, mainline, "", 1), "/", "", 1)
+		fmt.Printf("[%d] %s \n", index, ver)
+
+	}
+	fmt.Printf("\nChoose index: ")
+	var i int
+	fmt.Scan(&i)
+	u := links[i]
+
+	lst := GrabMainLinks(u)
+
+	m := map[string]bool{}
+	uniq := []string{}
+	for _, l := range lst {
+		if strings.Index(l, "_all") > -1 {
+			downloadlist = append(downloadlist, l)
+		}
+		if strings.Index(l, runtime.GOARCH+".deb") > -1 {
+			if strings.Index(l, "generic") > -1 {
+				downloadlist = append(downloadlist, l)
+
+			}
+		}
+	}
+	pth := downloadPath + fmt.Sprintf("%d", time.Now().Unix())
+	err := os.Mkdir(pth, 0777)
+	pe(err)
+
+	//rm dup
+	for v := range downloadlist {
+		if m[downloadlist[v]] != true {
+			m[downloadlist[v]] = true
+			uniq = append(uniq, downloadlist[v])
+			// fmt.Println(downloadlist[v])
+			// f := strings.Replace(downloadlist[v], mainline, "", 1)
+			// Download(downloadlist[v], pth+"/"+f)
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	for _, l := range uniq {
+		wg.Add(1)
+		f := strings.Split(strings.Replace(l, mainline, "", 1), "/")[1]
+		go Download(l, pth+"/"+f, &wg)
+
+	}
+	wg.Wait()
+
+	install(pth)
+
+}
+
+//Download download file
+func Download(u, dest string, wg *sync.WaitGroup) {
+	out, err := os.Create(dest)
+	pe(err)
+	fmt.Printf("file is downloading...\n")
+
+	defer out.Close()
+
+	response, err := http.Get(u)
+	we(err)
+	defer response.Body.Close()
+
+	bs, err := io.Copy(out, response.Body)
+	we(err)
+
+	fmt.Println(bs, "file downloaded.")
+	l(u, "Downloaded", dest)
+	defer wg.Done()
+
+}
+
+func install(dest string) {
+	files, err := ioutil.ReadDir(dest)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, v := range files {
+		fname := v.Name()
+		cmd := exec.Command("/usr/bin/sudo", "dpkg", "-i", fname) //dependency bug when installing header will be fixed
+		cmd.Dir = dest
+		by, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if cmd.ProcessState.Success() {
+			fmt.Println(string(by))
+			l("installed", v.Name())
+		}
+
+	}
+}
 func main() {
-
-	DisplayMenu()
-
+	// DisplayMenu()
+	DownloadKernel()
 }
